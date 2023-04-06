231d19a4221e1dbc26080c5e4eac456a905533e1
Alexi <38815390+alyoshenka@users.noreply.github.com>
2023-04-06 04:39:01 -0700

    22 connectionstatus icon (#25)
    * connection color
    
    * chip with color

--- src/app/aws/ConnectionStatus.jsx
+++ src/app/aws/ConnectionStatus.jsx
@@ -1,28 +1,53 @@
+/* eslint-disable max-len */
 /* eslint-disable no-console */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
-import { Button } from '@aws-amplify/ui-react';
+import Chip from '@mui/material/Chip';
+import { 
+  green, 
+  yellow, 
+  orange, 
+  red, 
+  blueGrey,
+} from '@mui/material/colors';
 import { Hub } from 'aws-amplify';
 import { CONNECTION_STATE_CHANGE } from '@aws-amplify/pubsub';
+import { SvgIcon } from '@mui/material';
 
 function ConnectionStatus() {
+  const colorDefault = blueGrey;
+  const colorMap = {
+    Connected: green, // Connected and working with no issues.
+    ConnectedPendingDisconnect: colorDefault, // The connection has no active subscriptions and is disconnecting.
+    ConnectedPendingKeepAlive: colorDefault, // The connection is open, but has missed expected keep alive messages.
+    ConnectedPendingNetwork: colorDefault, // The connection is open, but the network connection has been disrupted. When the network recovers, the connection will continue serving traffic.
+    Connecting: yellow, // Attempting to connect.
+    ConnectionDisrupted: orange, // The connection is disrupted and the network is available.
+    ConnectionDisruptedPendingNetwork: colorDefault, // The connection is disrupted and the network connection is unavailable.
+    Disconnected: red, // Connection has no active subscriptions and is disconnecting.
+  };
+
   const [connectionState, setConnectionState] = useState(undefined);
+  const [connectionColor, setConnectionColor] = useState(colorDefault);
 
   useEffect(() => {
     // todo: abstract into a function
     Hub.listen('pubsub', (data) => {
       const { payload } = data;
       if (payload.event === CONNECTION_STATE_CHANGE) {
-        setConnectionState(payload.data.connectionState);
+        const newState = payload.data.connectionState;
+        setConnectionState(newState);
+        setConnectionColor(colorMap[newState]);
       }
     });   
-  }, []);
+  }, [connectionState, connectionColor]);
 
   return (
     <div>
       <p>ConnectionState: {connectionState === undefined ? 'undefined' : connectionState}</p>
+      <Chip label={connectionState} style={{ backgroundColor: connectionColor[500] }} />
     </div>
   );
 }
