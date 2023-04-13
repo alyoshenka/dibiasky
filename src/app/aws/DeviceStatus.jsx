57ec22926a617a5eaaa9ad1196a626b5786d4293
Alexi <alexibmost@gmail.com>
2023-04-13 08:39:36 -0700

    device status icon

--- src/app/aws/DeviceStatus.jsx
+++ src/app/aws/DeviceStatus.jsx
@@ -2,18 +2,27 @@
 import React, { useState, useEffect } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
+import Chip from '@mui/material/Chip';
+import { blueGrey, green } from '@mui/material/colors';
 import { subscribe, addEntryToLog } from '../utils/utils';
 import { deviceConnected, deviceDisconnected } from '../utils/topics';
 
 function DeviceStatus({ deviceId, deviceName }) {
   const [isConnected, setIsConnected] = useState(false);
+  // todo: just use isConnected
+  const [connectionMessage, setConnectionMessage] = useState('Disconnected');
+  const [connectionColor, setConnectionColor] = useState(blueGrey[500]);
 
   const onConnected = (topic, payload) => {
     setIsConnected(true);
+    setConnectionMessage('Connected');
+    setConnectionColor(green[500]);
     addEntryToLog(`${deviceId} connected`); // not working??
   };
   const onDisconnected = (topic, payload) => {
     setIsConnected(false);
+    setConnectionMessage('Disconnected');
+    setConnectionColor(blueGrey[500]);
     addEntryToLog(`${deviceId} disconnected`);
   };
 
@@ -23,10 +32,12 @@ function DeviceStatus({ deviceId, deviceName }) {
   }, []);
 
   return (
-    <h3>
-      {`${deviceName} Status: `}
-      {isConnected ? 'Connected' : 'Disconnected'}
-    </h3>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <h4>
+        {`${deviceName} Status: `}
+      </h4>
+      <Chip label={connectionMessage} style={{ backgroundColor: connectionColor, alignSelf: 'center' }} />
+    </div>
   );
 }
 
--- src/app/utils/topics.js
+++ src/app/utils/topics.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-multi-spaces */
 export const publish = 'pub_test';
 export const subscribe = 'sub_test';
 
@@ -9,5 +10,5 @@ const hubbleOperations = 'dt/neo/operations/hubble';
 export const reqHubbleOperations = `${hubbleOperations}/req`;
 export const resHubbleOperations = `${hubbleOperations}/res`;
 
-export const deviceConnected = '$aws/events/presence/connected'; // '+'
+export const deviceConnected    = '$aws/events/presence/connected'; // '+'
 export const deviceDisconnected = '$aws/events/presence/disconnected';
