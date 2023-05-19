0b2bb10f52c93e012091c46a7027690d3822b169
Alexi <alexibmost@gmail.com>
2023-05-19 08:29:04 -0700

    table

--- src/app/aws/ScheduledOperations.jsx
+++ src/app/aws/ScheduledOperations.jsx
@@ -1,6 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 import { subscribe, publish, addEntryToLog } from '../utils/utils';
 import { scheduledOperationsReq, scheduledOperationsRes } from '../utils/topics';
 
@@ -38,26 +46,39 @@ function ScheduledOperations({ isConnected }) {
           addEntryToLog('Received bad Scheduled Operations');
         }
       });
-      // publish(scheduledOperationsReq, null);
+      publish(scheduledOperationsReq, null);
     }
   }, [isConnected]);
 
   useEffect(() => {
     const map = scheduledDB.map((sched, idx) => {
-      const executionTime = () => (sched.executeAt ? sched.executeAt : 'No time given');
-      const displayName = () => (sched.operation.friendlyName ? sched.operation.friendlyName : 'No operation given');
-      const content = (
-        <p>{`${executionTime()} : ${displayName()}`}</p>
+      // todo: display in a nicer output format
+      const executionTime = sched.executeAt ? sched.executeAt : 'No time given';
+      const displayName = sched.operation.friendlyName ? sched.operation.friendlyName : 'No operation given';
+      return (
+        // eslint-disable-next-line react/no-array-index-key
+        <TableRow key={idx}>
+          <TableCell>{executionTime}</TableCell>
+          <TableCell>{displayName}</TableCell>
+        </TableRow>
       );
-      // eslint-disable-next-line react/no-array-index-key
-      return <li key={idx}>{content}</li>;
     });
     setOperationsMap(map);
   }, [scheduledDB]);
   return (
     <div>
       <h3>Scheduled Operations</h3>
-      {operationsMap}
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Execution Time</TableCell>
+            <TableCell>Operation</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {operationsMap}
+        </TableBody>
+      </Table>
     </div>
   );
 }
