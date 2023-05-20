b00f99ef86cab61f0882bf8969d690252e494514
Alexi <alexibmost@gmail.com>
2023-05-20 06:00:57 -0700

    create and display timepicker component

--- src/app/operations/OperationScheduler.jsx
+++ src/app/operations/OperationScheduler.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@mui/material';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers';
+import { DatePicker, LocalizationProvider, TimePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import {
   addEntryToLog,
@@ -11,12 +10,35 @@ import {
 import { scheduleCommandReq, scheduleCommandRes, hubbleCommandReq } from '../utils/topics';
 
 function OperationScheduler({ operation }) {
-  const [commandTime, setCommandTime] = useState('click to initialize');
+  const [commandTime, setCommandTime] = useState('Click to initialize');
 
-  const updateCommandTime = () => {
+  const oneMinuteAhead = () => {
     const oneMin = new Date(new Date().getTime() + 1 * 60000);
     setCommandTime(oneMin.toISOString());
     // updateOptionsDict('executeAt', oneMin.toISOString());
+  };
+
+  const parseISOString = (s) => {
+    const b = s.split(/\D+/);
+    // eslint-disable-next-line no-plusplus
+    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+  };
+
+  const updateDate = (date) => {
+    const obj = new Date(date.$d);
+    const newDate = parseISOString(commandTime);
+    newDate.setDate(obj.getDate());
+    newDate.setMonth(obj.getMonth());
+    newDate.setYear(obj.getFullYear());
+    setCommandTime(newDate.toISOString());
+  };
+
+  const updateTime = (time) => {
+    const obj = new Date(time.$d);
+    const newTime = parseISOString(commandTime);
+    newTime.setHours(obj.getHours());
+    newTime.setMinutes(obj.getMinutes());
+    setCommandTime(newTime.toISOString());
   };
 
   const scheduleOperation = () => {
@@ -35,12 +57,19 @@ function OperationScheduler({ operation }) {
       <Button style={{ alignSelf: 'flex-start' }} onClick={scheduleOperation} variant="contained">Run Later</Button>
       <div style={{ marginTop: '7%' }}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateTimePicker label="Pick a time" />
+          {/* DateTimePicker was not allowing to select time */}
+          <DatePicker label="Pick a date" onChange={(val) => updateDate(val)} />
+          <TimePicker label="Pick a time" onChange={(val) => updateTime(val)} />
         </LocalizationProvider>
       </div>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <p><b>{commandTime}</b></p>
-        <Button onClick={updateCommandTime}>1 minute from now</Button>
+        <p>
+          <b>{commandTime}</b>
+          {'  ['}
+          {parseISOString(commandTime).toString()}
+          ]
+        </p>
+        <Button onClick={oneMinuteAhead}>1 minute from now</Button>
       </div>
     </div>
   );
