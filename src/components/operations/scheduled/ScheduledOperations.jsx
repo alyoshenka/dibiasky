a18f9bef48266fe05e772c8c86f0114dcefe6561
Alexi <alexibmost@gmail.com>
2023-05-30 09:46:20 -0700

    pretty format date

--- src/components/operations/scheduled/ScheduledOperations.jsx
+++ src/components/operations/scheduled/ScheduledOperations.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 import {
   Table,
   TableBody,
   TableCell,
-  TableContainer,
   TableHead,
   TableRow,
-  Button,
 } from '@mui/material';
 import DeleteScheduledOperation from './DeleteScheduledOperation';
 import { scheduledOperationsReq, scheduledOperationsRes } from '../../../utils/topics';
@@ -18,6 +17,7 @@ import { publish, subscribe } from '../../../utils/pubsub';
 function ScheduledOperations({ isConnected }) {
   const scheduledToTable = (operations) => {
     const optionsDictToDisplayStr = (sched) => JSON.stringify(sched.operation.options);
+    const prettyDate = (date) => dayjs(date).format('MM/DD/YY hh:mm:ss A');
     const map = operations.map((sched, idx) => {
       // todo: display in a nicer output format
       const executionTime = sched.executeAt ? sched.executeAt : 'No time given';
@@ -25,11 +25,9 @@ function ScheduledOperations({ isConnected }) {
       return (
         // eslint-disable-next-line react/no-array-index-key
         <TableRow key={idx}>
-          <TableCell>{executionTime}</TableCell>
+          <TableCell>{prettyDate(executionTime)}</TableCell>
           <TableCell>{displayName}</TableCell>
-          <TableCell>
-            {optionsDictToDisplayStr(sched)}
-          </TableCell>
+          <TableCell>{optionsDictToDisplayStr(sched)}</TableCell>
           <TableCell>
             <DeleteScheduledOperation
               scheduleID={sched.scheduleID}
@@ -75,7 +73,7 @@ function ScheduledOperations({ isConnected }) {
           <TableRow>
             <TableCell>Execution Time</TableCell>
             <TableCell>Operation</TableCell>
-            <TableCell>Data?</TableCell>
+            <TableCell>Data</TableCell>
             <TableCell />
           </TableRow>
         </TableHead>
