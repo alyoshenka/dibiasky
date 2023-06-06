873a67488eddc5d5e3f59af7f5257527953a1af5
VictorHugo <victorhugo@VictorHugos-MacBook-Pro.local>
2023-06-06 11:58:35 -0700

    added demos to exmaple page

--- src/pages/Examples.jsx
+++ src/pages/Examples.jsx
@@ -3,8 +3,12 @@ import * as React from 'react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { Divider } from '@mui/material';
 import ClippedDrawer from '../components/clippedDrawer';
 import ResponsiveAppBar from '../components/appBar';
+import demo from '../components/demos/hello_world_demo.mov';
+import demo2 from '../components/demos/alexi_victor_demo.mov';
+import demo3 from '../components/demos/abcsDemo.mov';
 
 function ExamplesPage() {
   return (
@@ -26,9 +30,28 @@ function ExamplesPage() {
           padding: '5%',
         }}
       >
-        <Typography>
-          <h1>This is the EXAMPLES page</h1>
-        </Typography>
+        <Box>
+          <Typography textAlign="center">
+            <h1>Project Example</h1>
+          </Typography>
+          <Box>
+            <video src={demo2} width="100%" height="200" controls="controls" autoPlay="true" loop="true" muted />
+            <Divider
+              textAlign="center"
+              sx={{ marginBottom: '1%' }}
+            >
+              <h1>ABCs</h1>
+            </Divider>
+            <video src={demo3} width="100%" height="200" controls="controls" autoPlay="true" loop="true" muted />
+            <Divider
+              textAlign="center"
+              sx={{ marginBottom: '1%' }}
+            >
+              <h1>Hello World</h1>
+            </Divider>
+            <video src={demo} width="100%" height="200" controls="controls" autoPlay="true" loop="true" muted />
+          </Box>
+        </Box>
       </Box>
     </>
   );
