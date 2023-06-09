d84901641d6b327d65cffcbd64dab3668e278ced
Alexi <alexibmost@gmail.com>
2023-06-09 09:09:37 -0700

    loaded md file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import ReactMarkdown from 'react-markdown';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import ClippedDrawer from '../components/clippedDrawer';
 import ResponsiveAppBar from '../components/appBar';
+import aboutPath from './About.md';
 
 function AboutPage() {
+  const [content, setContent] = useState(null);
+  useEffect(() => {
+    fetch(aboutPath).then((res) => res.text()).then((text) => setContent(text));
+    console.log(content);
+  }, []);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <CssBaseline />
@@ -26,6 +35,14 @@ function AboutPage() {
         <Typography textAlign="center">
           <h1>About Project</h1>
         </Typography>
+        <p>What is this project, exactly?</p>
+        <p>
+          This project allows you to specify messages and have them be
+          displayed on an LED board, either instantaneously or at a scheduled
+          time in the future, even while away from your local network.
+        </p>
+        <p>How does it work?</p>
+        <ReactMarkdown>{content}</ReactMarkdown>
       </Box>
     </Box>
   );
---
+++ src/pages/About.md
@@ -0,0 +1,33 @@
+## About
+
+giant page with all the gooey stuff
+
+### What is this project, exactly?
+
+This project allows you to specify messages and have them be displayed on an LED board, either instantaneously or at a scheduled time in the future, even while away from your local network. 
+
+### How does it work?
+
+This project is made up of four main parts:
+- Neo
+- Neopolitan
+- Dibiasky
+- AWS Code
+
+add explanation^
+
+#### Components and Architecture
+
+add diagrams
+
+#### Repositories
+
+### Additional Reading
+- developer log page
+- issue trackers
+- initial project spec?
+
+- struggles?
+- links
+- objectives?
+- contributors? or a different page
