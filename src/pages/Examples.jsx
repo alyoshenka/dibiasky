20260188244c9ad605d25762e52ddc1f36ffd3cd
VictorHugo <victorhugo@VictorHugos-MacBook-Pro.local>
2023-06-14 10:20:32 -0700

    changes to home page

--- src/pages/Examples.jsx
+++ src/pages/Examples.jsx
@@ -6,9 +6,9 @@ import Typography from '@mui/material/Typography';
 import { Divider } from '@mui/material';
 import ClippedDrawer from '../components/clippedDrawer';
 import ResponsiveAppBar from '../components/appBar';
-import demo from '../components/demos/hello_world_demo.mov';
-import demo2 from '../components/demos/alexi_victor_demo.mov';
-import demo3 from '../components/demos/abcsDemo.mov';
+import helloworld from '../components/demos/hello_world_demo.mov';
+import alexivictor from '../components/demos/alexi_victor_demo.mov';
+import abcs from '../components/demos/abcsDemo.mov';
 
 function ExamplesPage() {
   return (
@@ -30,27 +30,25 @@ function ExamplesPage() {
           padding: '5%',
         }}
       >
+        <Typography textAlign="center">
+          <h1>Project Example</h1>
+        </Typography>
         <Box>
-          <Typography textAlign="center">
-            <h1>Project Example</h1>
-          </Typography>
-          <Box>
-            <video src={demo2} width="100%" height="200" controls="controls" autoPlay="true" loop="true" muted />
-            <Divider
-              textAlign="center"
-              sx={{ marginBottom: '1%' }}
-            >
-              <h1>ABCs</h1>
-            </Divider>
-            <video src={demo3} width="100%" height="200" controls="controls" autoPlay="true" loop="true" muted />
-            <Divider
-              textAlign="center"
-              sx={{ marginBottom: '1%' }}
-            >
-              <h1>Hello World</h1>
-            </Divider>
-            <video src={demo} width="100%" height="200" controls="controls" autoPlay="true" loop="true" muted />
-          </Box>
+          <video src={alexivictor} width="100%" height="200" autoPlay="true" loop="true" muted />
+          <Divider
+            textAlign="center"
+            sx={{ marginBottom: '1%' }}
+          >
+            <h1>ABCs</h1>
+          </Divider>
+          <video src={abcs} width="100%" height="200" autoPlay="true" loop="true" muted />
+          <Divider
+            textAlign="center"
+            sx={{ marginBottom: '1%' }}
+          >
+            <h1>Hello World</h1>
+          </Divider>
+          <video src={helloworld} width="100%" height="200" autoPlay="true" loop="true" muted />
         </Box>
       </Box>
     </>
